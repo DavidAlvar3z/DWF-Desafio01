46aadf87d6e8bed2cc5ceba3e287b23f9341c688
DavidAlvar3z <AM240104@alumno.udb.edu.sv>
2025-08-01 18:40:30 -0600

    Add BookController REST endpoints for CRUD and title search
    Defined REST endpoints to interact with the Book entity:
    GET all books, POST to create, GET search by title, and DELETE by ID.
    Uses BookService for logic delegation.

--- bookapi/src/main/java/com/letrasvivas/bookapi/controller/BookController.java
+++ bookapi/src/main/java/com/letrasvivas/bookapi/controller/BookController.java
@@ -1,4 +1,40 @@
 package com.letrasvivas.bookapi.controller;
 
+import com.letrasvivas.bookapi.model.Book;
+import com.letrasvivas.bookapi.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String title) {
+        return bookService.findBooksByTitle(title);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+    }
 }
